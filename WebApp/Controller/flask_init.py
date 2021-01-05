c286f162f341dd8aa6d1367fa12351c3974bbfc1
pjk1413 <pjk1413@gmail.com>
2021-01-05 17:59:07 -0600

    CORS policy addressed

--- WebApp/Controller/flask_init.py
+++ WebApp/Controller/flask_init.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app, resources={r'/*': {'origins': '*'}})
+    # CORS(app)
     app.config.from_mapping(
         # a default secret that should be overridden by instance config
         SECRET_KEY="dev",
